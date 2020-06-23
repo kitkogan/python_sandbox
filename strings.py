5612dc40e1827592b1954d4b4fe6913e5bfc6814
Kit Kogan <kkogan.dev@gmail.com>
2020-06-23 14:30:57 -0500

    add string methods and variables

--- strings.py
+++ strings.py
@@ -1,6 +1,45 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = 'Kit'
+age = 36
+# Concatenate
+print('Hello I am ' + name + ' and I am ' + str(age))
+
 
 # String Formatting
 
+# Arguments by position
+# print('{}, {}, {}'.format('a', 'b', 'c'))
+# print('{1}, {2}, {0}'.format('a', 'b', 'c'))
+
+# Arguments by name
+# print('My name is {name} and I am {age}'.format(name='Kit', age='36'))
+
+# F-Strings (only in 3.6+)
+# print(f'my name is {name} and I am {age}')
+
 # String Methods
+
+s = 'hello there world'
+
+# Capitalize 1st letter
+print(s.capitalize())
+
+# All upper case
+print(s.upper())
+
+# All lower case
+print(s.lower())
+
+# Swap case
+print(s.swapcase())
+
+# Get Length
+print(len(s))
+
+# Replace
+print(s.replace('world', 'everyone'))
+
+# Count
+sub = 'h'
+print(s.count(sub))
